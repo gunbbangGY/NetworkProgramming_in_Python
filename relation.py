9cf678d1909d70f98b6f113d57b00ea23808d5e3
GunY <144870108+gunbbangGY@users.noreply.github.com>
2025-03-14 23:48:16 +0900

    Add files via upload

---
+++ relation.py
@@ -0,0 +1,6 @@
+exam = 85
+report = 85
+exam>=80 and report>=80
+exam>=80 or report>=80
+exam<90
+not(exam<90)
---
+++ string.py
@@ -0,0 +1,4 @@
+str1 = "single quote 'inside string'"
+str2 = 'double quote "inside string"'
+str3 = "double quote \"inside string\""
+print("{0:s} {1:s} {2:s}".format(str1, str2, str3))
