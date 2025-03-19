46b1ee4356d81e949413c4bc92381027319b0ecc
GunY <144870108+gunbbangGY@users.noreply.github.com>
2025-03-19 15:56:48 +0900

    Add files via upload

---
+++ option_sentence.py
@@ -0,0 +1,12 @@
+score = int(input('input num : '))
+if score >= 90 :
+    print("A")
+    print("Reward")
+else :
+    print('fail')
+    
+age_0=22
+age_1=18
+age=int(input('your age? : '))
+if age<age_0 and age>age_1 :
+    print('18 < your age < 23')
---
+++ type.py
@@ -0,0 +1,5 @@
+a=int(input('input num1 : '))
+b=input('input num2 : ')
+aVar = True
+print(type(a), type(b), type(aVar))
+
